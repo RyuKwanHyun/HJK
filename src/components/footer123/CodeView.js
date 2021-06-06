b74549689a9642aa83517bae674daff4fc39ca35
RyuKwanHyun <sosto258@naver.com>
2021-06-06 17:44:57 +0900

    V2.5 Final Update

--- src/components/footer123/CodeView.js
+++ src/components/footer123/CodeView.js
@@ -9,12 +9,6 @@ const CodeView = () => {
             <a className="code_link" href="https://github.com/RyuKwanHyun/HJK/tree/main">CODE VIEW</a>
 
             <div className="code_text-box">
-                <div className="code_contents">
-                    소스를 열람해보시고 사용하실 분은 사용하세요.
-                </div>
-                <div className="code_contents2">
-                    부족한 부분이나 지적도 환영입니다. ^__^
-                </div>
             </div>
 
         </div>
